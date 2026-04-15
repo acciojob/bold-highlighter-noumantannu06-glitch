6d1078ee4aabc5a6d00c024500c342d62d900589
Md Nouman <noumantannu06@gmail.com>
2026-04-15 15:40:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
-
+	const strongElements = document.querySelectorAll('strong');
+	strongElements.forEach(element => {
+		element.style.color = 'rgb(0,128,0)';
+	});
 
 }
 
 
 function return_normal() {
     //Write your code here
+	const strongElements = document.querSelectorAll('strong');
+	strongElements.forEach(element => {
+		element.style.color ='rgb(0, 0, 0,)';
+	});
 
     
 }
